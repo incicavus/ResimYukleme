5d2604567c44adb755c1c83e7d3643ccbd1fdacc
ivan0961 <incivan_09@hotmail.com>
2016-08-29 01:35:42 +0300

    First commit

---
+++ ResimFormu/ListeFormu.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ResimFormu
+{
+    partial class ListeFormu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMarka = new System.Windows.Forms.ListBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.pbResim = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbMarka
+            // 
+            this.lbMarka.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbMarka.FormattingEnabled = true;
+            this.lbMarka.ItemHeight = 18;
+            this.lbMarka.Location = new System.Drawing.Point(33, 30);
+            this.lbMarka.Name = "lbMarka";
+            this.lbMarka.Size = new System.Drawing.Size(171, 202);
+            this.lbMarka.TabIndex = 0;
+            this.lbMarka.SelectedIndexChanged += new System.EventHandler(this.lbMarka_SelectedIndexChanged);
+            // 
+            // txtModel
+            // 
+            this.txtModel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtModel.Location = new System.Drawing.Point(267, 30);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(177, 24);
+            this.txtModel.TabIndex = 1;
+            // 
+            // txtYil
+            // 
+            this.txtYil.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYil.Location = new System.Drawing.Point(267, 72);
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(177, 24);
+            this.txtYil.TabIndex = 2;
+            // 
+            // pbResim
+            // 
+            this.pbResim.Location = new System.Drawing.Point(267, 144);
+            this.pbResim.Name = "pbResim";
+            this.pbResim.Size = new System.Drawing.Size(109, 88);
+            this.pbResim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbResim.TabIndex = 3;
+            this.pbResim.TabStop = false;
+            // 
+            // ListeFormu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 291);
+            this.Controls.Add(this.pbResim);
+            this.Controls.Add(this.txtYil);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.lbMarka);
+            this.Name = "ListeFormu";
+            this.Text = "ListeFormu";
+            this.Load += new System.EventHandler(this.ListeFormu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbMarka;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtYil;
+        private System.Windows.Forms.PictureBox pbResim;
+    }
+}
---
+++ ResimFormu/ListeFormu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResimFormu
+{
+    public partial class ListeFormu : Form
+    {
+        public ListeFormu()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"server=WIN7-BILGISAYAR\SQLEXPRESS; database=net11db; integrated security=true");
+        private void ListeFormu_Load(object sender, EventArgs e)
+        {
+            Markalar();
+        }
+
+        void Markalar()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("select * from Arabalar", con);
+            DataSet ds = new DataSet();
+
+            adp.Fill(ds, "Arabalar");
+
+            lbMarka.DisplayMember = "Marka";
+            lbMarka.ValueMember = "ArabaID";
+            lbMarka.DataSource = ds.Tables["Arabalar"];
+        }
+
+        private void lbMarka_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("select * from Arabalar where ArabaID=@id", con);
+            adp.SelectCommand.Parameters.AddWithValue("@id", lbMarka.SelectedValue);
+
+            #region Eğer AddWithValue SQL Tarafında Hata verirse (tür uyumsuzluğu hatası ortaya çıkarsa)
+            //SqlParameter p1 = new SqlParameter("@id", SqlDbType.Int);
+            //p1.Value = (int)lbMarka.SelectedValue; 
+            #endregion
+
+            DataSet ds = new DataSet();
+            adp.Fill(ds);//bu haliyle ds içinde isimsiz bir tablo var
+
+            txtModel.Text = ds.Tables[0].Rows[0]["Model"].ToString();
+            txtYil.Text = ds.Tables[0].Rows[0]["Yil"].ToString();
+
+            string resimYolu = ds.Tables[0].Rows[0]["Foto"].ToString();
+
+            try
+            {
+                Bitmap bmp = new Bitmap(resimYolu);
+                pbResim.Image = bmp;
+            }
+            catch{ }
+
+
+
+        }
+    }
+}
